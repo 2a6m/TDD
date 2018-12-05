eac02d7021f2db8edc2a6ef2e4f7663ee294eddd
ChristopheSim <c.christophesimon@gmail.com>
2018-12-05 10:16:25 +0100

    Change the constructor

--- libraries/src/main/java/Main.java
+++ libraries/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 import backend.*;
 
 @SuppressWarnings("checkstyle:methodnamecheck")
-public class Main {
+private class Main {
     public static void main(String[] args) {
 
         SentenceMystery typeMystery = new SentenceMystery();
