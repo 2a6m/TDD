160b0b43578587802f579b7bf1b1947f625f854e
Jordan-JACOBI <32301879+Jordan-JACOBI@users.noreply.github.com>
2018-12-05 09:58:28 +0100

    Correct arg of SuppressWarnings

--- libraries/src/main/java/Main.java
+++ libraries/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import backend.*;
 
-@SuppressWarnings("checkstyle:methodnamecheck")
+@SuppressWarnings("checkstyle:methodname")
 public class Main {
     public static void main(String[] args) {
 
