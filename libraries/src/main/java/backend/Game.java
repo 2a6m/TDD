d3eafc83fb21c633a67d305f215ceec43210b929
2a6m <maxime.bourguignon1996@gmail.com>
2018-12-12 12:00:10 +0100

    correction javadoc

--- libraries/src/main/java/backend/Game.java
+++ libraries/src/main/java/backend/Game.java
@@ -7,12 +7,45 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+/* Class Game.
+ *
+ * class representing the carcteristics of the game
+ */
 public class Game {
+    /** Player informations.
+     *
+     * @param player represent the informations of the player.
+     */
     private Player player;
+
+    /** difficulty.
+     *
+     * @param difficulty object of type Difficulty representing the difficulty of the game.
+     */
     private Difficulty difficulty;
+
+    /** typeMystery represent the type of game.
+     *
+     * @param typeMystery object of type TypeMystery representing the type of game.
+     */
     private TypeMystery typeMystery;
+
+    /** mystery.
+     *
+     * @param mystery object of type Pair representing the object to find.
+     */
     private Pair<String, List<String>> mystery;
+
+    /** letterDisplay.
+     *
+     * @param letterDisplay object of type ArrayList representing letters to display.
+     */
     private ArrayList<Character> letterDisplay;
+
+    /** bonusList.
+     *
+     * @param bonusList object of type ArrayList representing all the bonus the user can use.
+     */
     private ArrayList<TypeBonus> bonusList = new ArrayList<TypeBonus>();
 
     public Game(Difficulty difficulty) { //Ajouter Player
@@ -23,73 +56,73 @@ public class Game {
 
     /** To play an other gameMystery.
      *
-     * @return void
+     * @return void.
      */
     public void NextMystery() { typeMystery.NextMystery(this); }
 
     /** Modifies the letters to display through the interface.
      *
-     * @param  letterDisplay   the new letters to display
-     * @return void
+     * @param  letterDisplay   the new letters to display.
+     * @return void.
      */
     public void SetLetterDisplay(ArrayList<Character> letterDisplay) { this.letterDisplay = letterDisplay; }
 
     /** Get the letters to display through the interface.
      *
-     * @return an ArrayList with the letters to display
+     * @return an ArrayList with the letters to display.
      */
     public ArrayList<Character> GetLetterDisplay() { return this.letterDisplay; }
 
     /** Modifies the letters to display through the interface.
      *
-     * @param  letterDisplay   the new letters to display
-     * @return void
+     * @param  letterDisplay   the new letters to display.
+     * @return void.
      */
     public void SetTypeMystery(TypeMystery typeMystery) { this.typeMystery = typeMystery; }
 
     /** Modifies the Difficulty of the game.
      *
-     * @param  difficulty      the new difficulty that the user has chosen
-     * @return void
+     * @param  difficulty      the new difficulty that the user has chosen.
+     * @return void.
      */
     public void SetDifficulty(Difficulty difficulty) { this.difficulty = difficulty; }
 
     /** Gives the Difficulty of the game.
      *
-     * @return an object Difficulty representing the difficulty of the current game
+     * @return an object Difficulty representing the difficulty of the current game.
      */
     public Difficulty GetDifficulty() { return this.difficulty; }
 
     /** Changes the mystery to display through the interface.
      *
-     * @param  mystery      the new mystery to display (a map where the key is the response of the mystery and the value is the mystery)
-     * @return void
+     * @param  mystery      the new mystery to display (a map where the key is the response of the mystery and the value is the mystery).
+     * @return void.
      */
     public void SetMystery(Pair<String, List<String>> mystery) { this.mystery = mystery; }
 
     /** Gives the current mystery of the game.
      *
-     * @return void
+     * @return void.
      */
     public Pair<String, List<String>> GetMystery() { return mystery; }
 
     /** Gives the available bonuses.
      *
-     * @return          a ArrayList of TypeBonus representing the available bonuses
+     * @return          a ArrayList of TypeBonus representing the available bonuses.
      */
     public ArrayList<TypeBonus> GetBonusList() { return bonusList; }
 
     /** Gives the current player who is playing.
      *
-     * @return a Player object representing the current player
+     * @return a Player object representing the current player.
      */
     public Player GetPlayer() { return player; }
 
     /** Connect and initialize a player.
      *
-     * @param  pseudo   the pseudo of the player
-     * @param  password the password of the player
-     * @return void
+     * @param  pseudo   the pseudo of the player.
+     * @param  password the password of the player.
+     * @return void.
      */
     public void Login(String pseudo, String password) {
         JSONObject playerData = CheckLogin(pseudo,password);
@@ -116,9 +149,9 @@ public class Game {
 
     /** Check the pseudo and the password of a player.
      *
-     * @param  pseudo   the pseudo of the player
-     * @param  password the password of the player
-     * @return          a JSONObject representing the player's data
+     * @param  pseudo   the pseudo of the player.
+     * @param  password the password of the player.
+     * @return          a JSONObject representing the player's data.
      */
     private JSONObject CheckLogin(String pseudo, String password) {
         JSONObject objectNull = new JSONObject();
@@ -144,7 +177,7 @@ public class Game {
 
     /** Update the json which represent the player's data.
      *
-     * @return void
+     * @return void.
      */
     public void Save() {
         try {
