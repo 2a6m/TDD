8f0e298bc2481701f3a55bf37fcb3d0b50c476e9
Jordan-JACOBI <32301879+Jordan-JACOBI@users.noreply.github.com>
2018-12-12 11:48:48 +0100

    Added javadoc

--- libraries/src/main/java/backend/Player.java
+++ libraries/src/main/java/backend/Player.java
@@ -1,26 +1,64 @@
+/**
+* Logical part of the project
+*/
 package backend;
 
+/**
+* This class is used to set player characteristics.
+*/
 public class Player {
 
+    /**
+    * Pseudo of the player.
+    */
     private String pseudo;
+    
+    /**
+    * Amount of coins of the player.
+    */
     private int coins;
 
+    /**
+    * Player constructor.
+    *
+    * @param pseudo
+    */
     public Player(String pseudo) {
         this.pseudo = pseudo;
     }
 
+    /**
+    * Getter : pseudo of the player.
+    *
+    * @return the pseudo of the player
+    */
     public String GetPseudo() {
         return pseudo;
     }
 
+    /**
+    * Setter : pseudo of the player.
+    *
+    * @param pseudo new pseudo for the player
+    */
     public void SetPseudo(String pseudo) {
         this.pseudo = pseudo;
     }
 
+    /**
+    * Getter : amount of coins of the player.
+    *
+    * @return the amount of coins of the player
+    */
     public int GetCoins() {
         return coins;
     }
 
+    /**
+    * Setter : amount of coins of the player.
+    *
+    * @param coins the amount of coins of the player
+    */
     public void SetCoins(int coins) {
         this.coins = coins;
     }
