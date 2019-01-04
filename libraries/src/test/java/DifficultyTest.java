a6ef03684c9d653c0d38fa643a065a789a06306b
2a6m <maxime.bourguignon1996@gmail.com>
2019-01-04 22:43:18 +0100

    test

--- libraries/src/test/java/DifficultyTest.java
+++ libraries/src/test/java/DifficultyTest.java
@@ -11,6 +11,7 @@ public class DifficultyTest {
     @Before
     public void setUp() {
         difficulty = new Difficulty("easy");
+        player = new Player("test");
     }
     /*
     @Test(expected = Exception.class)
@@ -21,17 +22,34 @@ public class DifficultyTest {
     }
     */
 
+    // test type of difficulty not in the Map coinConfigurator
+    @test
+    public void SetDIfficultyNameException() {
+        private Difficulty testDifficulty;
+        boolean thrown = false;
+
+        try {
+            testDifficulty = new Difficulty("test");
+        } catch (Exception e) {
+            thrown = true;
+        }
+
+        assertTrue("Exception not raised", thrown);
+    }
+
+    // ERROR
     @Test
     public void SetNameException() {
         int name = -1;
         boolean thrown = false;
 
         try {
+            // error but don't throw exception ?
             difficulty.SetName(name);
         } catch (Exception e) {
             thrown = true;
         }
 
-        assertTrue("Exception not catched", thrown);
+        assertTrue("Exception not raised", thrown);
     }
 }
