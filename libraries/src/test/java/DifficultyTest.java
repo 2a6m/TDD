9eebbdb72fd4f976ccb2ef4e3bd2ecb57cac7944
2a6m <maxime.bourguignon1996@gmail.com>
2019-01-04 21:53:03 +0100

    tes

--- libraries/src/test/java/DifficultyTest.java
+++ libraries/src/test/java/DifficultyTest.java
@@ -6,15 +6,19 @@ import org.junit.Test;
 public class DifficultyTest {
     private Difficulty difficulty;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Before
     public void setUp() {
         difficulty = new Difficulty("easy");
     }
 
-    @Test(expected = Exception.class)
-    public void SetNameException() {
+    @Test
+    public void SetNameException() throws Exception {
         int name = -1;
 
+        thrown.expect(Exception.class);
         difficulty.SetName(name);
     }
 }
