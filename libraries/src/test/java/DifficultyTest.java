f8215ced0c902c96862f85d7fcb3961c87139e3c
2a6m <maxime.bourguignon1996@gmail.com>
2019-01-04 22:09:49 +0100

    tes

--- libraries/src/test/java/DifficultyTest.java
+++ libraries/src/test/java/DifficultyTest.java
@@ -10,11 +10,26 @@ public class DifficultyTest {
     public void setUp() {
         difficulty = new Difficulty("easy");
     }
-
+    /*
     @Test(expected = Exception.class)
     public void SetNameException() throws Exception {
         int name = -1;
 
         difficulty.SetName(name);
+    }
+    */
+
+    @Test
+    public void SetNameException() {
+        int name = -1;
+        Bool thrown = false;
+
+        try {
+            difficulty.SetName(name);
+        } catch (Exception as e) {
+            thrown = true;
+        }
+
+        assertTrue("No Exception raised", thrown);
     }
 }
