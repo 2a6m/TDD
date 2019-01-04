5809fd49f30ed269c60c71ddfa17c13a1155464a
2a6m <maxime.bourguignon1996@gmail.com>
2019-01-04 21:31:02 +0100

    add test

---
+++ libraries/src/test/java/DifficultyTest.java
@@ -0,0 +1,34 @@
+package backend;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DifficultyTest {
+    private Difficulty difficulty;
+
+    @Before
+    public void setUp() {
+        difficulty = new Difficulty("easy");
+    }
+
+    @Test(expected = Exception.class)
+    public void SetNameException() {
+        Int name = -1;
+
+        difficulty.SetName(name)
+    }
+
+    @Test(expected = Exception.class)
+    public void SetNameException() {
+        Int name = 0;
+
+        difficulty.SetName(name)
+    }
+
+    @Test(expected = Exception.class)
+    public void SetNameException() {
+        Int name = 10;
+
+        difficulty.SetName(name)
+    }
+}
