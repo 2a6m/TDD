6977d76726d6084891b2e450e8542d6572b99c59
2a6m <maxime.bourguignon1996@gmail.com>
2019-01-05 12:20:58 +0100

    correction

---
+++ libraries/src/test/java/GameTest.java
@@ -0,0 +1,34 @@
+package backend;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    private Difficulty difficulty;
+    private Player player;
+    private Game game;
+
+    @Before
+    public void setUp() {
+        difficulty = new Difficulty("easy");
+        player = new Player("test");
+        game = new Game(difficulty);
+    }
+
+    // test pseudo empty
+    @Test
+    public void LoginEmpty() {
+        boolean thrown = false;
+
+        try {
+            game.Login("", "pwd_test");
+        } catch (Exception e) {
+            thrown = true;
+        }
+
+        assertTrue("Exception not raised", thrown);
+    }
+
+}
