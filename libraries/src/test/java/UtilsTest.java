f83794794e660b721a19e2860f4e9435a6002ffd
2a6m <maxime.bourguignon1996@gmail.com>
2019-01-06 11:46:01 +0100

    correction

--- libraries/src/test/java/UtilsTest.java
+++ libraries/src/test/java/UtilsTest.java
@@ -29,7 +29,7 @@ public class UtilsTest {
         boolean thrown = true;
 
         try {
-            Utils.MixArrayEmpty(array);
+            Utils.MixArray(array);
         } catch (Exception e) {
             thrown = false;
         }
